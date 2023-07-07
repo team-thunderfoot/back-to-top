ba70254098ad9933354a5e46945c07429cb100a6
Julieta Fredes <julietal.fredes@gmail.com>
2023-07-07 10:57:41 +0200

    code updated

--- src/BackToTop.js
+++ src/BackToTop.js
@@ -1,65 +1,64 @@
-import JSUTIL from "@andresclua/jsutil";
-import { tf_debounce } from "@andresclua/debounce-throttle";
+import JSUTIL from "@andresclua/jsutil"
+import { tf_debounce } from "@andresclua/debounce-throttle"
 
 class BackToTop {
-  constructor(payload) {
-    this.DOM = {
-      activeClass: payload.activeClass, // CSS class applied when the link is active
-      links: document.querySelectorAll("." + payload.linkClass) ?? [], // Selects all elements with the specified class in payload.linkClass
-      distanceTrigger: payload.distanceTrigger, // Distance from the top of the page to trigger the link activation
-    };
+    constructor(payload) {
+        this.DOM = {
+            element: payload.element,
+            distanceTrigger: payload.distanceTrigger,
+        }
 
-    this.JSUTIL = new JSUTIL();
-    this.events();
-  }
+        this.activeClass = payload.activeClass // CSS class applied when the element is active
 
-  init() {}
-
-  events() {
-    // Attach events to links and scroll
-    if (!!this.DOM.links.length) {
-      // Add click event listener to each link
-      this.DOM.links.forEach((link) => {
-        this.clickEvent = link.addEventListener(
-          "click",
-          this.goBackToTop.bind(this)
-        );
-      });
-
-      // Add scroll event listener with debounce to check scroll position
-      window.addEventListener(
-        "scroll",
-        tf_debounce(() => {
-          this.checkScroll();
-        }, 150)
-      );
+        this.JSUTIL = new JSUTIL()
+        this.events()
     }
-  }
 
-  checkScroll() {
-    // Check the scroll position and update link activation
-    this.DOM.links.forEach((link) => {
-      if (window.pageYOffset <= link.getAttribute(this.DOM.distanceTrigger)) {
-        this.JSUTIL.removeClass(link, this.DOM.activeClass); // Remove active class if scroll position is above the trigger point
-      } else {
-        this.JSUTIL.addClass(link, this.DOM.activeClass); // Add active class if scroll position is below the trigger point
-      }
-    });
-  }
+    init() {}
 
-  goBackToTop(e) {
-    // Scroll to the top of the page when a link is clicked
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-  }
+    events() {
+        // Attach events to the element and scroll
+        if (this.DOM.element) {
+            // Add click event listener to the element
+            this.clickEvent = this.DOM.element.addEventListener(
+                "click",
+                this.goBackToTop.bind(this)
+            )
 
-  destroy() {
-    // Remove event listener for the click event
-    this.clickEvent = {};
-  }
+            // Add scroll event listener with debounce to check scroll position
+            window.addEventListener(
+                "scroll",
+                tf_debounce(() => {
+                    this.checkScroll()
+                }, 150)
+            )
+        }
+    }
+
+    checkScroll() {
+        // Check the scroll position and update the element activation
+        if (
+            window.pageYOffset <= parseInt(this.DOM.element.getAttribute(this.DOM.distanceTrigger))
+        ) {
+            this.JSUTIL.removeClass(this.DOM.element, this.activeClass) // Remove active class if scroll position is above the trigger point
+        } else {
+            this.JSUTIL.addClass(this.DOM.element, this.activeClass) // Add active class if scroll position is below the trigger point
+        }
+    }
+
+    goBackToTop(e) {
+        // Scroll to the top of the page when the element is clicked
+        e.preventDefault()
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        })
+    }
+
+    destroy() {
+        // Remove event listener for the click event
+        this.clickEvent = {}
+    }
 }
-export default BackToTop;
+export default BackToTop
--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
-import BackToTop from "./BackToTop";
+import BackToTop from "./BackToTop"
 
 class Page {
-  constructor() {
-    this.init();
-    this.events();
-  }
-  init() {
-    const backToTop = new BackToTop({
-      linkClass: "js--back-top",
-      activeClass: "g--back-top-01--is-active",
-      distanceTrigger: "tf-data-distance",
-    });
+    constructor() {
+        this.init()
+        this.events()
+    }
+    init() {
+        document.querySelectorAll(".js--back-top").forEach((element) => {
+            const backToTop = new BackToTop({
+                element: element,
+                activeClass: "g--back-top-01--is-active",
+                distanceTrigger: "tf-data-distance",
+            })
+        })
 
-    // document
-    //   .querySelector(".js--destroy-top")
-    //   .addEventListener("click", (e) => {
-    //     e.preventDefault();
-    //     var destroy = this.backToTop.destroy();
-    //     console.log(destroy);
-    //   });
-  }
-  events() {}
+        // document
+        //   .querySelector(".js--destroy-top")
+        //   .addEventListener("click", (e) => {
+        //     e.preventDefault();
+        //     var destroy = this.backToTop.destroy();
+        //     console.log(destroy);
+        //   });
+    }
+    events() {}
 }
-export default Page;
-new Page();
+export default Page
+new Page()
